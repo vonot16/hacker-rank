48ad67462366a4b73394a64bacb0a077cc2f0a63
vonot16 <rafaelfeliperm@gmail.com>
2022-06-04 03:19:39 -0300

    Plus Minus Exercise

---
+++ javascript/plus-minus.js
@@ -0,0 +1,54 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+    let zero = 0
+    let positive = 0
+    let negative = 0
+    arr.forEach((e) =>{
+        if(e==0)
+            zero++
+        else if (e>0)
+            positive++
+        else
+            negative++
+    })
+    
+    console.log((positive/arr.length).toFixed(6))
+    console.log((negative/arr.length).toFixed(6))
+    console.log((zero/arr.length).toFixed(6))
+
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    plusMinus(arr);
+}
